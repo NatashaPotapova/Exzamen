aff38960f6b1e961b70e1eeca96984fcc10eaf66
NatashaPotapova <natusi-1999@mail.ru>
2020-04-25 13:23:16 +0300

    Добавление проекта

---
+++ Exzam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exzam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
